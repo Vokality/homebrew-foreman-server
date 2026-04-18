f02c70fe6d618f1933c94124ffedc9057a498fac
Lemi Boyce <lemuelboyce@gmail.com>
2026-04-18 14:15:16 -0400

    foreman-server 0.1.12

--- Formula/foreman-server.rb
+++ Formula/foreman-server.rb
@@ -5,15 +5,15 @@
 class ForemanServer < Formula
   desc "Foreman's local node runtime"
   homepage "https://github.com/Vokality/foreman"
-  version "0.1.11"
+  version "0.1.12"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Vokality/foreman/releases/download/server-v0.1.11/foreman-server-0.1.11-aarch64-apple-darwin.tar.gz"
-      sha256 "0a40e6d04e2377e523c21911bba8a75537200ae06217507a3e3e11623b08d3ee"
+      url "https://github.com/Vokality/foreman/releases/download/server-v0.1.12/foreman-server-0.1.12-aarch64-apple-darwin.tar.gz"
+      sha256 "9fe308979e279181a638c2844f64ff8503a676b2888825a6d77af83896a51a4e"
     else
-      url "https://github.com/Vokality/foreman/releases/download/server-v0.1.11/foreman-server-0.1.11-x86_64-apple-darwin.tar.gz"
-      sha256 "2e9f96b1e163fbdc6b3f61097e9d8f3d0947fa1bf36fa44f5170aa2d59895e25"
+      url "https://github.com/Vokality/foreman/releases/download/server-v0.1.12/foreman-server-0.1.12-x86_64-apple-darwin.tar.gz"
+      sha256 "19394abc653e6b96bc6d98c710cc38cc05c28a05e8f0e57850d07800c45dada9"
     end
   end
 
@@ -31,7 +31,7 @@ class ForemanServer < Formula
   service do
     run [opt_bin/"foreman-server", "run"]
     keep_alive true
-    environment_variables FOREMAN_HOME: var/"foreman-server", PATH: std_service_path_env
+    environment_variables PATH: std_service_path_env
   end
 
   test do
